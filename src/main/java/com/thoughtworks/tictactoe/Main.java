e1722ccf82215111d34ba5aa9b31954c5b77a920
suyaliu <suya1122@gmail.com>
2016-08-11 13:30:49 -0500

    init commit

--- src/main/java/com/thoughtworks/tictactoe/Main.java
+++ src/main/java/com/thoughtworks/tictactoe/Main.java
@@ -2,6 +2,6 @@ package com.thoughtworks.tictactoe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Tic Tac Toe");
+        System.out.println("Hello Tic Tac Toe it cool");
     }
 }
